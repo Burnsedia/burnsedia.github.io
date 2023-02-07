5e74d0000af993e948e66a9003063958a2f66e97
Burnsedia <bburnsed1995@gmail.com>
2023-02-07 10:44:14 -0500

    stoping the migration to gatsby for now, going to finish latter

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "gatsby";
+
+
+
+const NavBar = () => {
+  return (
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+
+
+  </nav>
+  )
+}
