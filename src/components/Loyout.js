1da7ae1172466ebe5709425bb45771e6cb469382
Burnsedia <bburnsed1995@gmail.com>
2023-02-13 20:10:44 -0500

    started working on the layout

---
+++ src/components/Loyout.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { Link } from "gatsby";
+import Footer from "./Footer"
+import Header from "./Header"
+const Layout = ({pageTitle, childern}) => {
+  
+  return (
+    <div>
+      <Header/>
+      <main>
+        <h1>{pageTitle}</h1>
+        {childern}
+      </main>
+      <Footer/>
+    </div>
+  )
+
+}
