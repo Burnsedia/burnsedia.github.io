0044bc1418a6bbca22ded18f19a57fe3f8a89676
Burnsedia <bburnsed1995@gmail.com>
2023-02-13 20:09:57 -0500

    started working on the footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+const Footer = () => {
+  return (
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+       mail@baileyburnsed.dev 
+      </p>
+    </div>
+</footer>
+  )
+
+}
