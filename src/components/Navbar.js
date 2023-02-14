4647dd91b11908eb439b8191e266efec1cdbcd69
Burnsedia <bburnsed1995@gmail.com>
2023-02-13 20:11:42 -0500

    started working on the Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,35 @@
 import React from "react";
-import Link from "gatsby";
+import { Link } from "gatsby";
 
 
 
 const NavBar = () => {
   return (
-  <nav class="navbar" role="navigation" aria-label="main navigation">
-
-
-  </nav>
+      <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          
+          <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+      
+        <div id="navbarBasicExample" class="navbar-menu centered">
+          <div class="navbar-start">
+            <Link to="/"class="navbar-item">
+              Home
+            </Link>
+      
+            <Link to="/projects" class="navbar-item">
+              Projects 
+            </Link>
+            <Link to="/blog" class="navbar-item">
+              Blog 
+            </Link>
+            </div>
+          </div>
+      </nav>
   )
 }
+
