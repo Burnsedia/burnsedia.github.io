883a40181292c5b995780ec99e0cc43cddce3b1c
Burnsedia <bburnsed1995@gmail.com>
2023-05-20 03:53:48 -0400

    mvoing to using react mui

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,10 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 import image from "@astrojs/image";
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://baileyburnsed.dev',
-  integrations: [mdx(), sitemap(), image()]
+  integrations: [mdx(), sitemap(), image(), react()]
 });
