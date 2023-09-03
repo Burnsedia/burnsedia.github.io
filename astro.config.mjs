967adbf89130454c92477633b66ed42ff07dd448
Burnsedia <bburnsed1995@gmail.com>
2023-09-03 09:43:53 -0400

    backing up before moving to astro 3.x

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 import image from "@astrojs/image";
 import react from "@astrojs/react"
+// import netlify from ""
 
 // https://astro.build/config
 export default defineConfig({
