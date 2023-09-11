166d7ba73e4c6308aec0293859fadddc4ef0bf30
Burnsedia <bburnsed1995@gmail.com>
2023-09-11 19:31:46 -0400

    setting tailwind css, so I can make this loook good

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
-import image from "@astrojs/image";
-import react from "@astrojs/react"
-// import netlify from ""
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://baileyburnsed.dev',
-  integrations: [react(),mdx(), sitemap()]
+  integrations: [react(), mdx(), sitemap(), tailwind()]
 });
