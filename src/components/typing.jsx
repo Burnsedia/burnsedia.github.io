18d54ecfb2169cd5b98eb787933dffdeef9ac26d
Burnsedia <bburnsed1995@gmail.com>
2024-02-07 02:35:32 -0500

    add svg logos

--- src/components/typing.jsx
+++ src/components/typing.jsx
@@ -5,13 +5,11 @@ export const Typing = () => {
     <TypeAnimation className='text-white'
       sequence={[
         // Same substring at the start will only be typed out once, initially
-        'Games in Godot',
-        1000, // wait 1s before replacing "Mice" with "Hamsters"
         'Mobile Apps in Flutter',
         1000,
-        'CLI Tools in Rust',
+        'Beautiful Website in Astro',
         1000,
-        'SaaS in Django',
+        'SaaS in Django and Vuejs',
         1000
       ]}
       wrapper="span"
