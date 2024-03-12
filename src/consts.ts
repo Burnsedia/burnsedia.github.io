75dbd1827699aae106edbe18c0d5c96ab78bd00d
Burnsedia <bburnsed1995@gmail.com>
2024-03-12 18:30:48 -0400

    fixxed typo

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'baileyburnsed.dev';
-export const SITE_DESCRIPTION = 'I Build Sotware for $997/month';
+export const SITE_DESCRIPTION = 'I Build Software for $997/month';
