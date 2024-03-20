664d783c6b40c70b291eedf93acb0244e6404121
Burnsedia <bburnsed1995@gmail.com>
2024-03-20 18:14:08 -0400

    added google google-anaylitics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import react from "@astrojs/react";
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -11,5 +10,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://baileyburnsed.dev',
-  integrations: [react(), mdx(), sitemap(), tailwind()]
+  integrations: [react(), mdx(), sitemap(), tailwind(), partytown(),
+]
 });
+
