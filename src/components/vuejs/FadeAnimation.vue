6073314d0f7cbdb0b96fbc8fdd80d72aa0f73c94
Burnsedia <bburnsed1995@gmail.com>
2024-04-09 16:07:43 -0400

    cleaned up my dirctory a bit

---
+++ src/components/vuejs/FadeAnimation.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div v-motion-fade>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<!-- <script> -->
+<!-- import { defineComponent } from 'vue'; -->
+<!-- import { useMotionFade } from '@vueuse/motion'; -->
+
+<!-- export default defineComponent({ -->
+<!--   setup() { -->
+<!--     const { fade } = useMotionFade(); -->
+
+<!--     return { -->
+<!--       fade, -->
+<!--     }; -->
+<!--   }, -->
+<!-- }); -->
+<!-- </script> -->
