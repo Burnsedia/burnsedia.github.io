d13b812dfbce08d860ea0c2ae60f7d5ec6a005f6
Burnsedia <bburnsed@gmail.com>
2024-11-25 22:37:40 -0500

    this should work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,44 +1,19 @@
-// import { defineConfig } from 'astro/config';
-// import mdx from '@astrojs/mdx';
-// import sitemap from '@astrojs/sitemap';
-// import react from "@astrojs/react";
-// import partytown from '@astrojs/partytown';
-
-// // https://astro.build/config
-// import tailwind from "@astrojs/tailwind";
-
-// // https://astro.build/config
-// export default defineConfig({
-//   site: 'https://baileyburnsed.dev',
-//   integrations: [react(), mdx(), sitemap(), tailwind(), partytown(),
-// ]
-// });
-
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 import partytown from '@astrojs/partytown';
-import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+   service: passthroughImageService(),
+ },
   site: 'https://baileyburnsed.dev',
-  integrations: [
-    react(),
-    mdx(),
-    sitemap(),
-    tailwind(),
-    partytown(),
-  ],
-  // Additional configurations for better performance
-  build: {
-    output: 'static', // Ensures a static site build
-  },
-  vite: {
-    optimizeDeps: {
-      include: ['sharp'], // Ensures sharp is bundled if required
-    },
-  },
+  integrations: [react(), mdx(), sitemap(), tailwind(), partytown(),
+]
 });
 
