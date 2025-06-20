cdeaf596d909ab42ec4477cc3452b4c1fc21189e
Burnsedia <bburnsed1995@gmail.com>
2025-06-19 23:48:13 -0400

    added a vuejs systm

---
+++ src/components/Typing.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { onMounted } from 'vue';
+import Typewriter from 'typewriter-effect/dist/core';
+
+onMounted(() => {
+  new Typewriter('#typewriter-vue', {
+    strings: ['Mobile', 'Web'],
+    autoStart: true,
+    loop: true,
+    delay: 75,
+    deleteSpeed: 50,
+  });
+});
+</script>
+
+<template>
+  <div id="typewriter-vue" class="text-white text-6xl px-3"></div>
+</template>
