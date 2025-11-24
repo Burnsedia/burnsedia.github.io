f2d8b103eff1a174d2f196ba469a892b552c72d7
Burnsedia <bburnsed1995@gmail.com>
2025-11-24 18:09:54 -0500

    This sould be faster

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'baileyburnsed.dev';
-export const SITE_DESCRIPTION = 'I Build Software for:';
+export const SITE_DESCRIPTION = 'I Build Software for: Web, Mobile, AI and More ';
 export const BLOG_DESCRIPTION = 'BLog';
