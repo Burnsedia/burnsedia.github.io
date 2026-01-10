5933c63c7e49eb26eaf8b64df0fe68baed3f840a
Burnsedia <bburnsed1995@gmail.com>
2026-01-09 23:20:41 -0500

    Add courses and projects collections to config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -18,8 +18,37 @@ const blog = defineCollection({
   }),
 });
 
+const courses = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    updatedDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+    heroImage: z.string().optional(),
+  }),
+});
 
+const projects = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    updatedDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+    heroImage: z.string().optional(),
+  }),
+});
 
-
-export const collections = { blog };
+export const collections = { blog, courses, projects };
 
