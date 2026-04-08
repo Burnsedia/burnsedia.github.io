bf6d3fe122f8e60f5f997bce77f173c17d1133d7
Burnsedia <bburnsed1995@gmail.com>
2026-04-07 23:22:32 -0400

    refactroing slop

--- src/content.config.ts
+++ src/content.config.ts
@@ -59,22 +59,4 @@ const courses = defineCollection({
   }),
 });
 
-const projects = defineCollection({
-  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/projects' }),
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z
-      .string()
-      .or(z.date())
-      .transform((val) => new Date(val)),
-    updatedDate: z
-      .string()
-      .optional()
-      .transform((str) => (str ? new Date(str) : undefined)),
-    heroImage: z.string().optional(),
-    tags: z.array(z.string()).optional(),
-  }),
-});
-
-export const collections = { blog, courses, projects };
+export const collections = { blog, courses };
